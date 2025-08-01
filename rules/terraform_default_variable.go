f911497bf81361256d75eb96d9b71c858036a71d
lundybch <christian.lundy@ahead.com>
2025-08-01 15:12:18 -0400

    ci: test

--- rules/terraform_default_variable.go
+++ rules/terraform_default_variable.go
@@ -2,7 +2,6 @@ package rules
 
 import (
   "github.com/terraform-linters/tflint-plugin-sdk/tflint"
-  "github.com/terraform-linters/tflint-plugin-sdk/schema"
 )
 
 type VariableDefaultRule struct{}
@@ -11,30 +10,15 @@ func NewVariableDefaultRule() *VariableDefaultRule {
   return &VariableDefaultRule{}
 }
 
-func (r *VariableDefaultRule) Name() string {
-  return "custom_variable_default"
-}
-
-func (r *VariableDefaultRule) Enabled() bool {
-  return true
-}
-
-func (r *VariableDefaultRule) Severity() schema.Severity {
-  return schema.ERROR
-}
-
-func (r *VariableDefaultRule) Link() string {
-  return ""
-}
+func (r *VariableDefaultRule) Name() string { return "custom_variable_default" }
+func (r *VariableDefaultRule) Enabled() bool { return true }
+func (r *VariableDefaultRule) Severity() tflint.Severity { return tflint.ERROR }
+func (r *VariableDefaultRule) Link() string { return "" }
 
 func (r *VariableDefaultRule) Check(runner tflint.Runner) error {
-  return runner.WalkBlocks("variable", func(block *schema.Block) error {
+  return runner.WalkBlocks("variable", func(block *tflint.Block) error {
     if _, exists := block.Body.Attributes["default"]; !exists {
-      return runner.EmitIssue(
-        r,
-        "Variable should have a default value",
-        block.DefRange,
-      )
+      return runner.EmitIssue(r, "Variable should have a default value", block.DefRange)
     }
     return nil
   })
