58c61aea93079128c572f8cfe1b39e2474ed0948
lundybch <christian.lundy@ahead.com>
2025-08-01 15:08:34 -0400

    ci: test

---
+++ rules/terraform_default_variable.go
@@ -0,0 +1,41 @@
+package rules
+
+import (
+  "github.com/terraform-linters/tflint-plugin-sdk/tflint"
+  "github.com/terraform-linters/tflint-plugin-sdk/tflint/schema"
+)
+
+type VariableDefaultRule struct{}
+
+func NewVariableDefaultRule() *VariableDefaultRule {
+  return &VariableDefaultRule{}
+}
+
+func (r *VariableDefaultRule) Name() string {
+  return "custom_variable_default"
+}
+
+func (r *VariableDefaultRule) Enabled() bool {
+  return true
+}
+
+func (r *VariableDefaultRule) Severity() schema.Severity {
+  return schema.ERROR
+}
+
+func (r *VariableDefaultRule) Link() string {
+  return ""
+}
+
+func (r *VariableDefaultRule) Check(runner tflint.Runner) error {
+  return runner.WalkBlocks("variable", func(block *schema.Block) error {
+    if _, exists := block.Body.Attributes["default"]; !exists {
+      return runner.EmitIssue(
+        r,
+        "Variable should have a default value",
+        block.DefRange,
+      )
+    }
+    return nil
+  })
+}
