0b8d6c92ca764062f995b4af3a3fbf4fb8c84896
lundybch <christian.lundy@ahead.com>
2025-08-01 15:21:51 -0400

    ci: test

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/terraform-linters/tflint-plugin-sdk/plugin"
 	"github.com/terraform-linters/tflint-plugin-sdk/tflint"
-	"github.com/terraform-linters/tflint-ruleset-template/rules"
+	"github.com/terraform-linters/tflint-ruleset/rules"
 )
 
 func main() {
@@ -16,6 +16,7 @@ func main() {
 				rules.NewAwsS3BucketExampleLifecycleRule(),
 				rules.NewGoogleComputeSSLPolicyRule(),
 				rules.NewTerraformBackendTypeRule(),
+				rules.NewTerraformDefaultVariableRule(),
 			},
 		},
 	})
