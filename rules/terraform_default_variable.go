5ee3884e8b5dd3980a419f7c45d23ed92652cc54
lundybch <christian.lundy@ahead.com>
2025-08-01 15:21:15 -0400

    ci: test

--- rules/terraform_default_variable.go
+++ rules/terraform_default_variable.go
@@ -1,55 +1,48 @@
 package rules
 
 import (
-	"github.com/hashicorp/hcl/v2"
-	"github.com/terraform-linters/tflint-plugin-sdk/tflint"
+  "fmt"
+  "github.com/terraform-linters/tflint-plugin-sdk/tflint"
+  "github.com/zclconf/go-cty/cty"
+  "github.com/hashicorp/hcl/v2"
 )
 
-type VariableDefaultRule struct{}
-
-func NewVariableDefaultRule() *VariableDefaultRule {
-	return &VariableDefaultRule{}
+type VariableHasDefaultRule struct {
+  tflint.DefaultRule
 }
 
-func (r *VariableDefaultRule) Name() string {
-	return "custom_variable_default"
-}
+func (r *VariableHasDefaultRule) Name() string     { return "custom_variable_has_default" }
+func (r *VariableHasDefaultRule) Enabled() bool    { return true }
+func (r *VariableHasDefaultRule) Severity() string { return tflint.ERROR }
+func (r *VariableHasDefaultRule) Link() string     { return "" }
 
-func (r *VariableDefaultRule) Enabled() bool {
-	return true
-}
+func (r *VariableHasDefaultRule) Check(runner tflint.Runner) error {
+  modules, err := runner.GetModuleContent(&tflint.BodySchema{
+    Blocks: []tflint.BlockSchema{
+      {
+        Type:       "variable",
+        LabelNames: []string{"name"},
+        Body: &tflint.BodySchema{
+          Attributes: []tflint.AttributeSchema{{Name: "default"}},
+        },
+      },
+    },
+  }, nil)
+  if err != nil {
+    return err
+  }
 
-func (r *VariableDefaultRule) Severity() tflint.Severity {
-	return tflint.ERROR
-}
-
-func (r *VariableDefaultRule) Link() string {
-	return ""
-}
-
-func (r *VariableDefaultRule) Metadata() *tflint.RuleMetadata {
-	return &tflint.RuleMetadata{
-		Version: "0.1.0",
-	}
-}
-
-func (r *VariableDefaultRule) Check(runner tflint.Runner) error {
-	return runner.WalkTerraformBlocks("variable", func(block *tflint.Block) error {
-		body, _, diag := runner.GetBodyContent(block.Body, nil)
-		if diag.HasErrors() {
-			return diag
-		}
-
-		// Check for the "default" attribute
-		attr, exists := body.Attributes["default"]
-		if !exists {
-			runner.EmitIssue(
-				r,
-				"Terraform variable is missing a default value",
-				block.DefRange,
-			)
-		}
-
-		return nil
-	})
+  for _, module := range modules {
+    for _, block := range module.Blocks {
+      varName := block.Labels[0]
+      _, hasDefault := block.Body.Attributes["default"]
+      if !hasDefault {
+        runner.EmitIssue(r,
+          fmt.Sprintf("variable %q has no default value", varName),
+          block.Body.Range,
+        )
+      }
+    }
+  }
+  return nil
 }
